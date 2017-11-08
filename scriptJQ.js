bc93486239ecc66f084ec9dfaf8734c14c8367fc
Lisa Gerken <lisamgerken@yahoo.com>
2017-11-08 08:54:19 -0500

    JQuery Lab Part 2

--- scriptJQ.js
+++ scriptJQ.js
@@ -1,21 +1,53 @@
 $(document).ready(function() {
-
-	$(".available").hover(function() {
-		$(this).fadeTo(1000, 0.50);	
+	var name;
+	var available;
+	var usersName;
+	var thisEl;
+	var partyNum;
+	var phone;
+	var party;
+	
+	$("p").hover(function() {
+		if ($(this).hasClass("available")) {
+			$(this).fadeTo(1000, 0.50);	
+		}
 	}, function() {
 		$(this).fadeTo(500, 1);
-	});
-	
-	$(".available").on("click", function() {
-		$(".formBox").css("visibility", "visible");
-	});	
-	
-	$(".reserved").click(function() {
-		$(this).css("cursor", "not-allowed");
+		
 	});
 	
 	$(".exitOut").click(function() {
-		$(".formBox").css("display", "none");
+		$(".formBox").css("visibility", "hidden");
+	});
+
+	$("p").on("mouseenter", function(event) {
+		if ($(this).hasClass("reserved")) {
+			$(this).css("cursor", "not-allowed");
+		} 
+		console.log(event);
 	});
 	
+	$(".available").on("click", function(event) {
+		$(".formBox").css("visibility", "visible");
+		thisEl = this;
+	});
+	
+	$("#save-btn").on("click", function(event) {
+		usersName = $("#name-id").val();
+		phoneNum = $("#phone-id").val();
+		partyNum = $("#party-id").val();
+		$(".available").attr("name", usersName);
+		$(".available").attr("phone", phoneNum);
+		$(".available").attr("party", partyNum);
+		$(thisEl).removeClass("available").addClass("reserved");
+		$(".reserved").off("click");
+		$("<span></span>").appendTo(".reserved");
+		$("span").text("Reserved for: " + usersName + " Party: " + partyNum);
+		$("span").css("z-index", "1");
+		$(".formBox").hide();
+	});
+	
+	
 });
+
+	
